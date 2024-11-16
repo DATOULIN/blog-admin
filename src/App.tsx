309220d7ab413f2ac77c74d0dfe6f471d05d0549
linjl <1217297278@qq.com>
2024-11-16 21:50:52 +0800

    init

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState} from 'react'
+import {useEffect, useState} from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
@@ -9,7 +9,8 @@ function App() {
     const [count, setCount] = useState(0)
 
     useEffect(() => {
-        axiosInstance.get('/test').then(() => {})
+        axiosInstance.get('/test').then(() => {
+        })
     }, [])
 
     return (
@@ -34,6 +35,10 @@ function App() {
             <p className="read-the-docs">
                 Click on the Vite and React logos to learn more
             </p>
+            <button onClick={async () => {
+                await axiosInstance.post('/user/register', {username: 'xxxxx', password: 'xxxxx'})
+            }}>register
+            </button>
         </>
     )
 }
